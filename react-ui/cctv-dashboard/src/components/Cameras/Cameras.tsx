826453348b14deb9c5e36f6be26fc24d538d7ca4
Aaron <aaronteoh@hotmail.com>
2020-01-12 17:08:17 +0800

    Added input

--- react-ui/cctv-dashboard/src/components/Cameras/Cameras.tsx
+++ react-ui/cctv-dashboard/src/components/Cameras/Cameras.tsx
@@ -5,35 +5,50 @@ import { JSXElement } from "@babel/types";
 import Button from "react-bootstrap/Button";
 
 class Cameras extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   state = {
     players: {
       1: {
         show: true,
         label: "Camera 1",
-        url: "https://www.youtube.com/watch?v=1EiC9bvVGnk",
-        maximise: false
+        url: "https://drive.google.com/uc?export=download&id=1WLm1va6XreJAFh0u4lY5Ntza-Aj05j8W",
+        maximise: false,
+        showCam: "hidden",
+        showUrl: "show"
       },
       2: {
         show: true,
         label: "Camera 2",
         url: "https://www.youtube.com/watch?v=1EiC9bvVGnk",
-        maximise: false
+        maximise: false,
+        showCam: "hidden",
+        showUrl: "show"
       },
       3: {
         show: true,
         label: "Camera 3",
         url: "https://www.youtube.com/watch?v=1EiC9bvVGnk",
-        maximise: false
+        maximise: false,
+        showCam: "hidden",
+        showUrl: "show"
       },
       4: {
         show: true,
         label: "Camera 4",
         url: "https://www.youtube.com/watch?v=1EiC9bvVGnk",
-        maximise: false
+        maximise: false,
+        showCam: "hidden",
+        showUrl: "show"
       }
     },
+    url: "",
     showNav: false
   };
+
   click = id => {
     const players = Object.create(this.state.players);
     let showNav = false;
@@ -49,7 +64,7 @@ class Cameras extends Component {
   };
 
   render() {
-    const { players, showNav } = this.state;
+    const { players, showNav, url } = this.state;
     let currentPlayers: any = [];
     for (let key in players) {
       currentPlayers.push(
@@ -59,7 +74,9 @@ class Cameras extends Component {
           players[key].show,
           players[key].label,
           this.click,
-          players[key].maximise
+          players[key].controls,
+          players[key].maximise,
+          this
         )
       );
     }
@@ -70,28 +87,55 @@ class Cameras extends Component {
       </div>
     );
   }
+
+   handleChange(event) {
+    var players = this.state.players
+    players[event.target.id].url = event.target.value
+    this.setState({players});
+  }
+
+  handleSubmit(event){
+    //alert('A name was submitted: ' + this.state.players[event.target.id].url);
+    var players = this.state.players
+    players[event.target.id].showCam = "show"
+    players[event.target.id].showUrl = "hidden"
+    this.setState({players});
+
+
+    event.preventDefault();
+  }
 }
 
-function createCameras(url, id, show, label, click, maximise) {
+function createCameras(url, id, show, label, click, controls, maximise, context) {
   if (!show) return null;
   let cName: string = maximise ? "maximise-box-part" : "camera-box-part";
   let playerHeight: string = maximise ? "800px" : " 350px";
   return (
     <div className="col-xl-6">
       <a href="#" />
-      <div className={cName + " text-center"} onClick={() => click(id)}>
-        <Button className="text" variant="dark" size="lg">
+      <div className={cName + " text-center"}>
+        <Button className="text" variant="dark" size="lg" onClick={() => click(id)}>
           {label}
         </Button>
-        <ReactPlayer
-          width="100"
-          height={playerHeight}
-          url={url}
-          playing={true}
-        />
+        <form onSubmit={context.handleSubmit}  id={id} className={context.state.players[id].showUrl}>
+          <label>
+            URL:
+            <input type="text" id={id} onChange={context.handleChange}/>
+          </label>
+        </form>
+        <div className={context.state.players[id].showCam} >
+            <ReactPlayer
+              width="100"
+              height={playerHeight}
+              url={url}
+              controls={controls}
+              playing={true}
+            />
+        </div>
       </div>
     </div>
   );
 }
+
 
 export default Cameras;
